ab7a227279e52df830202c611bf9a00b7a9444a2
guilhermealvino <guilhermealvi9@gmail.com>
2022-04-17 08:59:52 -0300

    selectionSort - Estrutura de dados

---
+++ src/br/com/alura/algoritmos/TestaOrdenacao.java
@@ -0,0 +1,10 @@
+package br.com.alura.algoritmos;
+
+public class TestaOrdenacao {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ src/produtos/TestaMenorPreco.java
@@ -0,0 +1,35 @@
+package produtos;
+
+public class TestaMenorPreco {
+
+	public static void main(String[] args) {
+				  		
+		Produto produtos[] = {
+				new Produto("Lamboghini", 1000000),
+				new Produto("Jipe", 46000),
+				new Produto("Brasilia", 16000),
+				new Produto("Smart", 46000),
+				new Produto("Fusca", 17000),
+				null
+		};
+		
+		
+		int maisBarato = buscaMenor(produtos, 0, 4);
+		System.out.println(maisBarato);
+		System.out.println("O carro: " +produtos[maisBarato].getNome()
+									   + ", é o mais barato e custa: R$ " +produtos[maisBarato].getPreco());
+				
+	}
+
+	private static int buscaMenor(Produto[] produtos, int inicio, int termino) {
+		int maisBarato = inicio;
+		//int termino = (produtos.length - 1);
+		for(int atual = inicio; atual <= termino; atual++) {
+			if(produtos[atual].getPreco() < produtos[maisBarato].getPreco()) {
+				maisBarato = atual;
+			}
+		}
+		return maisBarato;
+	}
+
+}
